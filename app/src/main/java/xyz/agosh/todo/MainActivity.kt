cd77320cc096ade73664c3ba65ee639008385d4d
agosh <agageldidurdyyev@gmail.com>
2022-05-10 15:40:37 +0300

    Add Todo UI

--- app/src/main/java/xyz/agosh/todo/MainActivity.kt
+++ app/src/main/java/xyz/agosh/todo/MainActivity.kt
@@ -2,10 +2,19 @@ package xyz.agosh.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        setupActionBarWithNavController(findNavController(R.id.navigationHostFragment))
     }
+
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = findNavController(R.id.navigationHostFragment)
+//        return navController.navigateUp() || super.onSupportNavigateUp()
+//    }
 }
