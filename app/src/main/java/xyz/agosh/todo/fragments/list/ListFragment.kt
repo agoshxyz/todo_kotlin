794482df30025c12fd281be7bb17cc55eae84401
agosh <agageldidurdyyev@gmail.com>
2022-05-10 17:09:34 +0300

    menu list fragment

--- app/src/main/java/xyz/agosh/todo/fragments/list/ListFragment.kt
+++ app/src/main/java/xyz/agosh/todo/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package xyz.agosh.todo.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_list.view.*
 import xyz.agosh.todo.R
@@ -31,8 +29,15 @@ class ListFragment : Fragment() {
         view.listLayout.setOnClickListener { findNavController().navigate(R.id.action_listFragment_to_updateFragment) }
 
 
+        //Menu items
+        setHasOptionsMenu(true)
+
         return view
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 
 
